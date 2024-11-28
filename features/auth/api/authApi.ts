d51d6ca421affa59b55fecc89b620947ff698857
dkrasilnikov <krasilnikov.orchid@gmail.com>
2024-11-28 17:45:42 +0100

    feat: remove unused functions from api

--- features/auth/api/authApi.ts
+++ features/auth/api/authApi.ts
@@ -1,18 +1,6 @@
 import { Alert } from 'react-native';
 
 import { supabase } from '$shared/api/supabase';
-import { saveToken, deleteToken } from '$shared/store/asyncStore';
-
-export const signInWithPassword = async (email: string, password: string) => {
-  const { data, error } = await supabase.auth.signInWithPassword({
-    email,
-    password,
-  });
-
-  if (error) throw new Error(error.message);
-
-  await saveToken(data.session?.access_token || '');
-};
 
 export const signUpWithPassword = async (email: string, password: string) => {
   console.log('signup');
@@ -26,9 +14,4 @@ export const signUpWithPassword = async (email: string, password: string) => {
 
   if (error) throw new Error(error.message);
   if (!session) Alert.alert('Please check your inbox for email verification!');
-};
-
-export const signOut = async () => {
-  await supabase.auth.signOut();
-  await deleteToken();
 };

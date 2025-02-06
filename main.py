d5d16e1339df6ec5c9d0547180b618b14fcadde9
Svetlana Perekrestova <svetlana.perekrestova2@gmail.com>
2025-02-06 23:28:44 +0200

    NPE fix

--- main.py
+++ main.py
@@ -46,8 +46,8 @@ def extract_entities(req: TextRequest) -> Dict[str, List[Dict[str, Any]]]:
             print(entity_detail)
             if entity_detail:
                 umls_entities.append({
-                    "cui": entity_detail.CUI,
-                    "canonical_name": entity_detail.name,
+                    "cui": entity_detail.concept_id,
+                    "canonical_name": entity_detail.canonical_name,
                     "definition": entity_detail.definition,
                     "aliases": entity_detail.aliases,
                 })

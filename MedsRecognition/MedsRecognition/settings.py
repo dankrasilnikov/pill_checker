c8e43d32e38e857a2d64261b5ea0c978dd2f7ea7
Svetlana Perekrestova <svetlana.perekrestova2@gmail.com>
2025-02-08 03:14:11 +0200

    Fixing props resolution

--- MedsRecognition/MedsRecognition/settings.py
+++ MedsRecognition/MedsRecognition/settings.py
@@ -5,7 +5,7 @@ from decouple import config
 
 SECRET_KEY = "your-secret-key"
 DEBUG = True
-ALLOWED_HOSTS = [config("BIOMED_HOST", default="")]
+ALLOWED_HOSTS = ["165.232.73.98"]
 
 INSTALLED_APPS = [
     "django.contrib.sessions",

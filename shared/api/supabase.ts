a41dc1a9bedcae85cf43ad4ce9129b9747d6166f
Tirimar <tirimar.channel@gmail.com>
2025-02-11 01:23:39 +0100

    fix: delete old supabase

--- shared/api/supabase.ts
+++ shared/api/supabase.ts
@@ -3,10 +3,6 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
 import { AppState } from 'react-native';
 
-const SUPABASE_URL = 'https://vwcedmobksqqnsalktge.supabase.co';
-const SUPABASE_ANON_KEY =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ3Y2VkbW9ia3NxcW5zYWxrdGdlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzIzMjkxNTMsImV4cCI6MjA0NzkwNTE1M30.vc_XycBKWhH86c2lDjW8SWWQnyHfVwsqsy9mMp8kb68';
-
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
     storage: AsyncStorage,
@@ -15,8 +11,6 @@ export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
     detectSessionInUrl: false,
   },
 });
-
-console.log(supabase);
 
 AppState.addEventListener('change', (state) => {
   if (state === 'active') {
